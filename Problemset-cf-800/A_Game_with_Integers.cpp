084dae13de1a0f5afda96462326ffe721eb7f0c3
aeron-gh <agab0323@gmail.com>
2026-09-11 20:34:31 +0530

    cf-800

---
+++ Problemset-cf-800/A_Game_with_Integers.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    if (n % 3 == 0)
+    {
+        cout << "Second" << endl;
+    }
+    else
+    {
+        cout << "First" << endl;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
